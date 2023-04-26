0d4e179c64ecebcca70fc34f614d5b803c986e31
ivan camino <caminoivan904@gmail.com>
2023-04-26 09:05:05 -0300

    manejo-archivos

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 class ProductManager {
   constructor() {
     this.product = []
--- practica.js
+++ practica.js
@@ -1,4 +1,4 @@
-function suma(n1,n2) {
+/*function suma(n1,n2) {
 
  return new Promise(
     (resolve,reject) => {
@@ -73,8 +73,102 @@ return reject({
 }
     })
 }
-resta(21,34)
-resta(9,10)
-resta(9,20)
+
 calculos(30,20,resta).then(res=> console.log(res)).catch(err=> console.log(err))
-calculos(30,10,resta)
+calculos(30,10,resta)*/
+//practica clase manejos de sistemas js
+/*const fs = require('fs')
+let ruta = './nuevoArchivo.js'
+let objetito = JSON.stringify(['nuevo',{nombre:'ivan', apellido: 'camino'}],null,2)
+fs.promises.writeFile(ruta,objetito)
+.then(res=>console.log('archivo creado'))
+.catch(err=>console.log(err))
+fs.promises.unlink(ruta)*/
+//practica map js
+/*const numeros = [23,24,25,26,27,28]
+let resultado = numeros.map((value)=> value + 1)
+console.log(resultado)
+
+const alimentos= ['papa','arroz','salsa']
+
+function cocina(alimento) {
+    const clonacion = {
+        'papa': 'papas fritas',
+        'arroz': 'guiso',
+        'salsa': 'fideos'
+    }
+    return clonacion[alimento] || alimento
+}
+const comida = alimentos.map(cocina)
+console.log(comida)*/
+const fs = require('fs')
+class userManager {
+    constructor(path) {
+        this.users = []
+        this.path = path
+        this.init(path)
+    }
+    init(path) {
+let file = fs.existsSync(path)
+if (!file) {
+   fs.promises.writeFile(this.path,'[]')
+   console.log('file created at path'+this.path)
+  return 'file created at path'+this.path
+     }else {
+        this.users = JSON.parse(fs.readFileSync(this.path,'UTF-8'))
+        console.log('data recovered')
+        return 'data recovered'  
+        // fs.promises.readFile(path,'utf-8')
+        //.then(res=> this.user = JSON.parse(res))
+        //.then(res=> this.users = res)
+        //.catch(err=> console.log(err))
+     }
+   }  
+   addUser({ name, last_name,age,cart }) {
+       let claves = { name, last_name,age,cart }
+       claves.id = 1
+       console.log(this.users)
+       this.users.push(claves) //agrego un usuario a la memoria del programa
+     let claves_JSON = JSON.stringify(this.users,null,2)//pasar a formato JSON,stringifearlo el array entero
+       fs.promises.writeFile(this.path,claves_JSON) //y luego sobreescribo el archivo
+       .then(res=> console.log('user created'))
+       .catch(err=> console.log(err))
+       
+   }
+ }
+
+let manager = new userManager('./users.json')
+async function crud() {
+  await  manager.addUser({ name: 'marcos', last_name: 'lopez',age: 20,cart: [] })
+   await manager.addUser({ name: 'juan', last_name: 'lares',age: 25,cart: [] })
+   await manager.addUser({ name: 'beto', last_name: 'jauregui',age: 30,cart: [] })
+}
+
+crud()
+
+/* try {
+        //defino el objeto que necesito agregar al array
+        let data = { title,description,price,thumbnail,stock }
+        //si la memoria tiene usuarios
+        if (this.product.length>0) {
+            //busco el id del último elemento y le sumo 1
+            let next_id = this.product[this.product.length-1].id+1
+            //agrego la propiedad al objeto
+            data.id = next_id
+        } else {
+            //en caso que no tenga: asigno el primer id
+            data.id = 1
+        }
+        //agrego el objeto (usuario) a la memoria del programa
+        this.product.push(data)
+        //convierto a texto plano el array
+        let data_json = JSON.stringify(this.product,null,2)
+        //sobre-escribo el archivo
+        await fs.promises.writeFile(this.path,data_json)
+        console.log('id´s created user: '+data.id)
+        return 'id´s user: '+data.id
+    } catch(error) {
+        console.log(error)
+        return 'error: creating user'
+    }
+    }*/
